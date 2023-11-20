97add23dc339d36ad49bc3faa8749f8256541de6
johannes-gertje <96242790+johannes-gertje@users.noreply.github.com>
2023-11-20 09:02:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,36 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("This is my express app");
+  res.send("This is my express backend");
 });
 
 app.get("/me", (req, res) => {
-  res.send("Hi I am Laith");
+  res.send("Hi I am Backend");
 });
 
-app.listen(6000, () => {
+
+let rawdata_2 = '{ "employees" : [' +
+'{ "firstName":"John" , "lastName":"Doe" },' +
+'{ "firstName":"Anna" , "lastName":"Smith" },' +
+'{ "firstName":"Peter" , "lastName":"Jones" } ]}';
+
+let json_file_2 = JSON.parse(rawdata_2);
+console.log(json_file_2);
+
+app.get("/api/test",(req,res)=>{
+    res.setHeader("content-type","application/json");
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.status(200).send(json_file_2);
+})
+
+
+app.get('/api/test2', (req, res, next) => {
+    let colors = ["blue","yellow", "red"];
+    res.setHeader("content-type","application/json");
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.status(200).send(JSON.stringify(colors));
+});
+
+app.listen(4000, () => {
   console.log("backend listening");
 });
